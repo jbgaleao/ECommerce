dbcb1a052ce2d3bf701bff44a194614156d9de9f
Joao Galeao <jbgaleao@gmail.com>
2021-04-18 23:24:34 -0300

    Classe das cidades

---
+++ Models/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Name { get; set; }
+
+        public int DepartamentsId { get; set; }
+    }
+}
