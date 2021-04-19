bac192ffb364f49faaf0ad1c09f993e9aa5cd26a
Joao Galeao <jbgaleao@gmail.com>
2021-04-19 00:41:24 -0300

    Classe para configurar combobox

---
+++ Classes/CombosHelper.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Classes
+{
+    public class CombosHelper:IDisposable
+    {
+        private static ECommerceContext db = new ECommerceContext();
+        public static List<Departaments> GetDepartments()
+        {
+            IList<Departaments> dep = db.Departaments.ToList();
+            dep.Add(new Departaments
+            {
+                DepartamentsId = 0,
+                Name = "[ Selecione um Departamento ]"
+            });
+
+            dep = dep.OrderBy(d => d.Name).ToList();
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
